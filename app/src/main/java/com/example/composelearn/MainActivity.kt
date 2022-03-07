85281aada2bc35db9cb4e83bf74ec8be67514ee1
Tridiv <roytridiv1996@gmail.com>
2022-03-07 15:57:12 +0600

    Initial commit

---
+++ app/src/main/java/com/example/composelearn/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.composelearn
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composelearn.ui.theme.ComposeLearnTheme
+import kotlin.coroutines.coroutineContext
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeLearnTheme {
+                myApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    //Tweaking the UI
+    Surface(
+        color = MaterialTheme.colors.secondary,
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Column(modifier = Modifier.wrapContentSize(Alignment.Center)) {
+            for (item in createAList()) {
+                Column(modifier = Modifier.padding(2.dp)) {
+                    Text(text = item)
+                }
+            }
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(text = "test 1")
+                Text(text = "test 2")
+            }
+
+
+            Text(
+                text = "Hello $name!",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+            )
+        }
+
+    }
+
+}
+
+@Composable
+fun HelloWorld() {
+    val context = LocalContext.current
+    Surface(
+        color = MaterialTheme.colors.secondary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(14.dp)) {
+            Row (modifier = Modifier.weight(2f)) {
+                Text(text = "Hello, ")
+                Text(text = "Compose Learn Code")
+            }
+            Button(
+                onClick = {
+                    Toast.makeText( context, "Button Pressed", Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Text("Show more")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 300)
+@Composable
+fun DefaultPreview() {
+    ComposeLearnTheme {
+        myApp()
+    }
+}
+
+@Composable
+fun myApp() {
+    Surface(color = MaterialTheme.colors.background) {
+        HelloWorld()
+    }
+}
+
+fun createAList(): MutableList<String> {
+    return mutableListOf(
+        "test1",
+        "text2",
+        "text2",
+        "text2",
+        "text2",
+        "text2",
+        "text2",
+
+        )
+}
